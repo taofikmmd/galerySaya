d1dc00774cfe6fd41743491f67cb35356d402146
Taofik Dimas <158401549+taofikmmd@users.noreply.github.com>
2025-12-04 22:27:04 +0700

    Refactor modal logic and add slideshow functionality

--- script.js
+++ script.js
@@ -199,13 +199,13 @@ function createPhotoCard(doc) {
             
             <div class="mt-3 flex space-x-2">
                 <a href="${firstImageUrl}" download="${data.title || 'gambar'}.jpg" 
-                   class="inline-flex items-center justify-center w-2/3 bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300">
-                    ⬇️ Download
+                   class="inline-flex items-center justify-center w-2/3 bg-indigo-700 hover:bg-indigo-600 text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300">
+                     Download
                 </a>
 
                 <button data-id="${docId}" 
-                        class="delete-btn w-1/3 bg-red-500 hover:bg-red-600 text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300">
-                    🗑️ Hapus
+                        class="delete-btn w-1/3 bg-red-700 hover:bg-red-600 text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300">
+                     Hapus
                 </button>
             </div>
             
@@ -321,3 +321,4 @@ prevBtn.addEventListener('click', () => {
 
 // Panggil fungsi untuk memulai tampilan galeri
 loadAndListenForPhotos();
+
